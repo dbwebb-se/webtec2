56861c781cb6c720fde4d6b093e9202e03db3cba
Mikael Roos <mikael.t.h.roos@gmail.com>
2024-10-05 20:50:16 +0200

    Add counting points and show stats

--- lab/lab1/lab.js
+++ lab/lab1/lab.js
@@ -3,20 +3,97 @@
  * and to calculate the result.
  */
 
+const level = {
+  pass: 10,
+  honour: 13,
+}
+
+const feedback = {
+  0: `Try to earn 1 point to get started... 😏`,
+  1: `Nice work, lets go, do another! 😉`,
+  5: `Great, you are on fire and progress is made. 😋`,
+  9: `Just one more to PASS. Lets go. 😅`,
+  10: `Excellent, you have PASSED. Lets try one more? 😁`,
+  13: `That is the way, you PASSED WITH HONOUR! 😍`,
+  16: `What can I say. You impress me. 🙌`,
+}
+
+const stats = {}
+
 /**
  * Execute the testcase and assert that it was corrent or not and return a
  * status string the can be written out.
  * @param {Function} func - The function to test.
- * @param {Function} expected - The expected result from the function call.
- * @param {Function} args - The arguments to send to the function.
+ * @param {Object} expected - The expected result from the function call.
+ * @param {Object} args - The arguments to send to the function.
+ * @param {number} point - The number of points that this functions is worth in total.
  * @returns {string} A string representation of the test case success or failure.
  */
-export function assert (func, expected, args) {
+export function assert (func, expected, args, point=1) {
   const result = func(...args)
   const success = expected === result ? '✅' : '❌'
-  // check if array []
-  // print as string ''
-  // print out the arguments used in each testcase
-  // calculate the points
-  return `${success} [${func.name}] ${expected} (${typeof expected}) => ${result} (${typeof result})`
+
+  // Calculate the stats
+  if (!(func.name in stats)) {
+    stats[func.name] = {
+      run: 0,
+      passed: 0,
+      failed: 0,
+      point,
+    }
+  }
+  stats[func.name].run++
+  if (expected === result) {
+    stats[func.name].passed++
+  } else {
+    stats[func.name].failed++
+  }
+
+  // Prepare the argument string
+  let argStr = ''
+  for (let arg of args) {
+    if (typeof arg === 'string') {
+      argStr += `'${arg}', `
+    } else if (typeof arg === 'object' && Array.isArray(arg)) {
+        argStr += `[${arg}], `
+      } else {
+      argStr += `${arg}, `
+    }
+  }
+
+  return `${success} ${stats[func.name].point}p ${func.name}(${argStr.slice(0, -2)}), expected: ${expected} (${typeof expected}), actual: ${result} (${typeof result})`
+}
+
+/**
+ * Print out the results when the execution is done.
+ * @returns {string} A string representation of the results.
+ */
+export function done (func, expected, args) {
+  let points = 0
+  let passed = 0
+  let failed = 0
+  let total = 0
+  let result = 0
+  let str = ''
+  for (const func of Object.keys(stats)) {
+    points += stats[func].point
+    passed += stats[func].passed
+    failed += stats[func].failed
+    total += stats[func].passed + stats[func].failed
+    result += stats[func].point * Math.floor(stats[func].passed / (stats[func].passed + stats[func].failed), 1)
+  }
+
+  let msg = feedback[0]
+  for (const key of Object.keys(feedback)) {
+    if (result < key) {
+      break
+    }
+    msg = feedback[key]
+  }
+
+  str += `Passed: ${passed}, Failed: ${failed}, Total: ${total}`
+  str += `\nYou have ${result} points out of total ${points} points.`
+  str += `\nYou need ${level.pass} points to PASS and ${level.honour} points to PASS WITH HONOUR.`
+  str += `\n${msg}`
+  return str
 }
--- lab/lab1/main.js
+++ lab/lab1/main.js
@@ -6,7 +6,7 @@ import * as lab from './lab.js'
 import * as module from './module.js'
 
 console.log('Ready to go to work! Lets make it all green!\n' + '='.repeat(50))
-console.log(lab.assert(module.hello, 'Hello world', []))
+console.log(lab.assert(module.hello, 'Hello world', [], 0))
 
 console.log(lab.assert(module.magicNumber, 42, []))
 
@@ -29,19 +29,21 @@ console.log(lab.assert(module.sum, 6, [[1, 2, 3]]))
 console.log(lab.assert(module.sum, 1, [[1]]))
 console.log(lab.assert(module.sum, 0, [[]]))
 
-console.log(lab.assert(module.max, 6, [[1, 2, 3, 6, 5, 4]]))
+console.log(lab.assert(module.max, 6, [[1, 2, 3, 6, 5, 4]], 2))
 console.log(lab.assert(module.max, 1, [[1, -1]]))
 console.log(lab.assert(module.max, 10, [[10, 9, 8, -10]]))
 
 console.log(lab.assert(module.reverseString, 'cba', ['abc']))
 console.log(lab.assert(module.reverseString, 'leakiM', ['Mikael']))
 
-console.log(lab.assert(module.countWords, 4, ['Mumintrollet bor i skogen.']))
+console.log(lab.assert(module.countWords, 4, ['Mumintrollet bor i skogen.'], 2))
 console.log(lab.assert(module.countWords, 4, [' Hej jag heter Mikael ']))
 
-console.log(lab.assert(module.countVowels, 9, ['aeiouyåäö.']))
+console.log(lab.assert(module.countVowels, 9, ['aeiouyåäö.'], 2))
 console.log(lab.assert(module.countVowels, 8, ['Mumintrollet bor i skogen.']))
 
-console.log(lab.assert(module.isPalindrom, true, ['AbccbA']))
+console.log(lab.assert(module.isPalindrom, true, ['AbccbA'], 3))
 console.log(lab.assert(module.isPalindrom, true, ['AbcbA']))
 console.log(lab.assert(module.isPalindrom, false, ['Mumintroll']))
+
+console.log(lab.done())
